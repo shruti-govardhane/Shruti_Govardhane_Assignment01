d1227741a2b1a7e2719a7e4887a8a16b84f3dcef
shruti-govardhane <168740373+shruti-govardhane@users.noreply.github.com>
2024-06-26 14:53:57 +0530

    Pushed Task - 07

---
+++ Assignment -07/Controllers/EmployeeAdditionalDetailsController.cs
@@ -0,0 +1,60 @@
+﻿using Employee_Management_System.DTO;
+using Employee_Management_System.Interface;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Employee_Management_System.Controllers
+{
+
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class EmployeeAdditionalDetailsController: Controller
+    {
+        private readonly IEmployeeAdditionalDetails _employeeAdditionalDetails;
+
+        public EmployeeAdditionalDetailsController(IEmployeeAdditionalDetails employeeAdditionalDetails)
+        {
+            _employeeAdditionalDetails = employeeAdditionalDetails;
+        }
+
+        [HttpPost]
+
+        public async Task<EmployeeAdditionalDetailsDTO> Add_AdditionalData(EmployeeAdditionalDetailsDTO employeeAdditinalDetailsDTO)
+        {
+            var response = await _employeeAdditionalDetails.Add_AdditionalData(employeeAdditinalDetailsDTO);
+            return response;
+        }
+
+        [HttpGet]
+
+        public async Task<List<EmployeeAdditionalDetailsDTO>> GetAllEmployeeAdditionalData()
+        {
+            var response = await _employeeAdditionalDetails.GetAllEmployeeAdditionalData();
+            return response;
+        }
+
+       [HttpGet]
+
+        public async Task<EmployeeAdditionalDetailsDTO> GetEmployeeAdditionalDataByUId(string UId)
+        {
+            var response = await _employeeAdditionalDetails.GetEmployeeAdditionalDataByUId(UId);
+            return response;
+        }
+
+
+        [HttpPost]
+        public async Task<EmployeeAdditionalDetailsDTO> UpdateAdditionalData(EmployeeAdditionalDetailsDTO employeeAdditionalDetailsDTO)
+        {
+            var response = await _employeeAdditionalDetails.UpdateAdditionalData(employeeAdditionalDetailsDTO);
+            return response;
+        }
+
+        [HttpPost]
+        public async Task<string> DeleteEmployeeAdditional(string UId)
+        {
+            var response = await _employeeAdditionalDetails.DeleteEmployeeAdditional(UId);
+            return response;
+        }
+
+       
+    }
+}
---
+++ Assignment -07/CosmosDB/ICosmosDBService.cs
@@ -0,0 +1,24 @@
+﻿
+using Employee_Management_System.DTO;
+using Employee_Management_System.Entity;
+
+namespace Employee_Management_System.CosmosDB
+{
+    public interface ICosmosDBService
+    {
+        Task<EmployeeBasicDetailsEntity> AddEmployee(EmployeeBasicDetailsEntity employeeBasicDetailsEntity);
+     Task<List<EmployeeBasicDetailsEntity>> GetAllEmployee();
+
+        Task<EmployeeBasicDetailsEntity> GetEmployeeByUId(string UId);
+
+        Task ReplaceAsync(dynamic employee);
+
+
+        Task<EmployeeAdditionalDetailsEntity> Add_AdditionalData(EmployeeAdditionalDetailsEntity employeeAdditionalDetailsEntity);
+
+
+        Task<List<EmployeeAdditionalDetailsEntity>> GetAllEmployeeAdditionalData();
+
+       Task<EmployeeAdditionalDetailsEntity> GetEmployeeAdditionalDataByUId(string uId);
+    }
+}
